a86a055857fc3d77952e13aac747039645a75e57
pablo-nardi <nardipablo31@gmail.com>
2021-06-15 23:09:51 -0300

    Se empezo ejercicio de http

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { ArticlesService } from '../articles.service';
 
@@ -9,6 +10,7 @@ import { ArticlesService } from '../articles.service';
 export class ArticlesComponent implements OnInit {
 
   articles:any = []
+  artitags:any = [] //ARTICULOES PERTENECIENTES A UN TAG
   tags: any = [];
   constructor(private service: ArticlesService) { }
 
@@ -16,9 +18,28 @@ export class ArticlesComponent implements OnInit {
 
   }
   loadArticles() {
-    this.service.getArticles().subscribe(response => this.articles = response.articles);
+    this.service.getArticles().subscribe(response => {
+      this.articles = response.articles
+      console.log(this.articles);
+    });
+    
   }
   loadTags() {
-    this.service.getTags().subscribe(response => this.tags = response.tags);
+    this.service.getTags().subscribe(response =>{
+      this.tags = response.tags;
+      //console.log(this.tags);
+      this.tags.forEach(element => {
+        console.log(element);
+
+      });
+    });
+  }
+  loadArticlesByTag(tag:string){
+    console.log(`tocaste el tag --> ${tag}`);
+    this.service.getArticlesByTag(tag).subscribe(response =>{
+     this.artitags = response.articles;
+     this.articles = response.articles
+     console.log(this.artitags);
+    });
   }
 }
